f52a15ae098bb6ece85682c2af1e7c433ec38b4b
Tania <tania@efaber.net>
2012-05-09 10:25:32 +0200

    Initial commit. User model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeustoApp::Application.config.secret_token = '27df9c829af395361633391e75f1f7e4142daf700bfeb483acde69cdad980b8028de9d00e7b2dab919d366b2df693f281162970e81e1523aaf67a027b24f2e47'
